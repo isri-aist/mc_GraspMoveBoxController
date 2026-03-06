7b6c9a7bed4456d9f557883887d555a77861b2bd
Martin Filliung <martin.filliung@aist.go.jp>
2026-03-06 19:45:48 +0900

    wip: added a separate raise hand state for parellel execution

---
+++ src/states/MoveHands.cpp
@@ -0,0 +1,53 @@
+#include "MoveHands.h"
+#include "../DemoController.h"
+#include "utils.h"
+
+void MoveHands::configure(const mc_rtc::Configuration &config)
+{
+    mc_rtc::log::info("\n{}", config.dump(true, true));
+
+    // if (!(config.has("leftHandTargetPositionRobot") && config.has("rightHandTargetPositionRobot")))
+    //     mc_rtc::log::error_and_throw("Missing required config fields");
+
+    config("leftHandTargetPositionRobot", m_leftHandTargetPositionRobot);
+    config("rightHandTargetPositionRobot", m_rightHandTargetPositionRobot);
+
+    config("stiffness", m_stiffness);
+    config("weight", m_weight);
+    config("leftHandFrame", m_leftHandFrame);
+    config("rightHandFrame", m_rightHandFrame);
+}
+
+void MoveHands::start(mc_control::fsm::Controller &ctl_)
+{
+    auto &ctl = static_cast<DemoController &>(ctl_);
+
+    // for (auto &j : ctl.robot().mb().joints()) mc_rtc::log::info("{}", j.name());
+
+    m_leftGripperTask = std::make_shared<mc_tasks::RelativeEndEffectorTask>(
+            m_leftHandFrame, ctl.robots(), 0, m_robotReferenceFrame, m_stiffness, m_weight);
+    m_leftGripperTask->selectActiveJoints(ctl.solver(), LeftArmJoints);
+    m_leftGripperTask->set_ef_pose({m_leftHandTargetOrientationRobot, m_leftHandTargetPositionRobot});
+    ctl.solver().addTask(m_leftGripperTask);
+
+    m_rightGripperTask = std::make_shared<mc_tasks::RelativeEndEffectorTask>(
+            "RightHandWrench", ctl.robots(), 0, m_robotReferenceFrame, m_stiffness, m_weight);
+    m_rightGripperTask->selectActiveJoints(ctl.solver(), RightArmJoints);
+    m_rightGripperTask->set_ef_pose({m_rightHandTargetOrientationRobot, m_rightHandTargetPositionRobot});
+    ctl.solver().addTask(m_rightGripperTask);
+}
+
+bool MoveHands::run(mc_control::fsm::Controller &ctl_)
+{
+    if (!m_waitForEvalThreshold) return true;
+}
+
+void MoveHands::teardown(mc_control::fsm::Controller &ctl_)
+{
+    auto &ctl = static_cast<DemoController &>(ctl_);
+
+    ctl.solver().removeTask(m_leftGripperTask);
+    ctl.solver().removeTask(m_rightGripperTask);
+}
+
+EXPORT_SINGLE_STATE("MoveHands", MoveHands)
---
+++ src/states/MoveHands.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <Eigen/Geometry>
+#include <mc_control/fsm/State.h>
+#include <mc_tasks/RelativeEndEffectorTask.h>
+
+
+struct MoveHands : mc_control::fsm::State
+{
+        bool run(mc_control::fsm::Controller &ctl_) override;
+        void start(mc_control::fsm::Controller &ctl_) override;
+        void teardown(mc_control::fsm::Controller &ctl_) override;
+        void configure(const mc_rtc::Configuration &config) override;
+
+    protected:
+        Eigen::Vector3d m_leftHandTargetPositionRobot;
+        Eigen::Vector3d m_rightHandTargetPositionRobot;
+
+        Eigen::Quaterniond m_leftHandTargetOrientationRobot = {0.5, 0.5, 0.5, -0.5};
+        Eigen::Quaterniond m_rightHandTargetOrientationRobot  = {0.5, -0.5, 0.5, 0.5};
+
+        std::string m_robotReferenceFrame = "CHEST_Y_LINK";
+        std::string m_leftHandFrame       = "LeftHandSupportPlate";
+        std::string m_rightHandFrame      = "RightHandSupportPlate";
+
+        double m_stiffness = 2.0;
+        double m_weight    = 1000.0;
+
+        bool m_waitForEvalThreshold = false;
+
+        std::shared_ptr<mc_tasks::RelativeEndEffectorTask> m_leftGripperTask;
+        std::shared_ptr<mc_tasks::RelativeEndEffectorTask> m_rightGripperTask;
+};
