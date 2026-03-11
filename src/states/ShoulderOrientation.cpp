02684e2801b02f4c16cc12683c69b738d900341e
Martin Filliung <martin.filliung@aist.go.jp>
2026-03-11 16:09:19 +0900

    wip: testing

--- src/states/ShoulderOrientation.cpp
+++ src/states/ShoulderOrientation.cpp
@@ -14,36 +14,30 @@ void ShoulderOrientation::configure(const mc_rtc::Configuration &config)
 
     config("stiffness", m_stiffness);
     config("weight", m_weight);
-    config("leftShoulderZAngle", m_leftShoulderZAngle);
-    config("rightShoulderZAngle", m_rightShoulderZAngle);
+    config("leftShoulderAngle", m_leftShoulderAngle);
+    config("rightShoulderAngle", m_rightShoulderAngle);
 }
 
 void ShoulderOrientation::start(mc_control::fsm::Controller &ctl_)
 {
     auto &ctl = static_cast<DemoController &>(ctl_);
 
-    const auto &refFrameOri = ctl.robot().frame(m_robotReferenceFrame).position().rotation();
+    // for (auto &j : ctl.robot().mb().joints()) mc_rtc::log::info("{}", j.name());
 
-    m_leftElbowOrientationTask = std::make_shared<mc_tasks::OrientationTask>(
-            ctl.robot().frame(m_leftShoulderFrame), m_stiffness, m_weight / 2);
-    m_leftElbowOrientationTask->selectActiveJoints(ctl.solver(), m_leftShoulderActiveJoints);
-    m_leftElbowOrientationTask->orientation(refFrameOri * sva::RotZ(m_leftShoulderZAngle));
+    m_leftElbowOrientationTask =
+            std::make_shared<mc_tasks::OrientationTask>(ctl.robot().frame(m_leftShoulderFrame), m_stiffness, m_weight);
     ctl.solver().addTask(m_leftElbowOrientationTask);
 
-    m_rightElbowOrientationTask = std::make_shared<mc_tasks::OrientationTask>(
-            ctl.robot().frame(m_rightShoulderFrame), m_stiffness, m_weight / 2);
-    m_rightElbowOrientationTask->selectActiveJoints(ctl.solver(), m_rightShoulderActiveJoints);
-    m_rightElbowOrientationTask->orientation(refFrameOri * sva::RotZ(m_rightShoulderZAngle));
+    m_rightElbowOrientationTask =
+            std::make_shared<mc_tasks::OrientationTask>(ctl.robot().frame(m_rightShoulderFrame), m_stiffness, m_weight);
     ctl.solver().addTask(m_rightElbowOrientationTask);
+
+    setOrientationTaskGoal(ctl_);
 }
 
 bool ShoulderOrientation::run(mc_control::fsm::Controller &ctl_)
 {
-    auto &ctl = static_cast<DemoController &>(ctl_);
-
-    const auto &refFrameOri = ctl.robot().frame(m_robotReferenceFrame).position().rotation();
-    m_leftElbowOrientationTask->orientation(refFrameOri * sva::RotZ(m_leftShoulderZAngle));
-    m_rightElbowOrientationTask->orientation(refFrameOri * sva::RotZ(m_rightShoulderZAngle));
+    setOrientationTaskGoal(ctl_);
 
     // is meant to run parallel
     return true;
@@ -55,6 +49,23 @@ void ShoulderOrientation::teardown(mc_control::fsm::Controller &ctl_)
 
     ctl.solver().removeTask(m_leftElbowOrientationTask);
     ctl.solver().removeTask(m_rightElbowOrientationTask);
+}
+
+void ShoulderOrientation::setOrientationTaskGoal(mc_control::fsm::Controller &ctl_) const
+{
+    auto &ctl = static_cast<DemoController &>(ctl_);
+
+    auto &leftReferenceFrame  = ctl.robot().frame(m_leftReferenceFrame).position().rotation();
+    auto &rightReferenceFrame = ctl.robot().frame(m_rightReferenceFrame).position().rotation();
+
+    const Eigen::Matrix3d leftRotation  = sva::RotX(m_leftShoulderAngle);
+    const Eigen::Matrix3d rightRotation = sva::RotX(m_rightShoulderAngle);
+
+    const Eigen::Matrix3d leftFinal = leftRotation * leftReferenceFrame;
+    const Eigen::Matrix3d rightFinal = rightRotation * rightReferenceFrame;
+
+    m_leftElbowOrientationTask->orientation(leftRotation * leftReferenceFrame);
+    m_rightElbowOrientationTask->orientation(rightRotation * rightReferenceFrame);
 }
 
 EXPORT_SINGLE_STATE("ShoulderOrientation", ShoulderOrientation)
